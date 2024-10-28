e9358d930672a0f1ccaff64c03fadb4e11af2660
Ayushmishra05 <ayush89718@gmail.com>
2024-10-28 19:00:56 +0530

    added testcases

---
+++ mathopertest.py
@@ -0,0 +1,14 @@
+from src.mathoper import add, sub, multi 
+
+def testadd():
+    assert add(1 , 2) == 3
+    assert add(2 , 3) == 5
+
+
+def testsub():
+    assert sub(5 , 6) == -1
+    assert sub(4 , 5) == -1
+    assert sub(5 , 12) == -7
+
+
+
---
+++ src/mathoper.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return a +b 
+
+def sub(b , c):
+    return b - c 
+
+def multi(a , c):
+    return a *c 
